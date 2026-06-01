7bf5b5f90cae8f587d8447cd1efccda617f21203
Callum O'Donnell <cjodonnell1992@gmail.com>
2026-06-01 08:55:23 -0400

    fix nodemailer import

--- lib/email.ts
+++ lib/email.ts
@@ -1,4 +1,5 @@
-import nodemailer from 'nodemailer'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nodemailer = require('nodemailer')
 
 export async function sendPriceAlert({
   to,
